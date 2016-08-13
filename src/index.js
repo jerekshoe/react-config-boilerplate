572b34abc7df4054c1c3c4245b097de2f4c5c08a
jerekshoe <jereks88@gmail.com>
2016-08-13 13:31:12 -0500

    Name change complete

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import React from 'react';
 import { render } from 'react-dom';
 
 render(
-  <h1>Welcome to Redux-Boilerplate!</h1>,
+  <h1>Welcome to React-Config-Boilerplate!</h1>,
   document.getElementById('main')
 );
